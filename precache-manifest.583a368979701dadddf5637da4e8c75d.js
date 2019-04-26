ac833b88eb0b53d97dac2486fcc2b6afb607dbc7
woorian <woorian@siso.io>
2019-04-26 19:22:24 +0900

    Updates

---
+++ precache-manifest.583a368979701dadddf5637da4e8c75d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "cacc282f3a8941721106",
+    "url": "/woorian/DIYPAGE.git/static/js/main.9988f8aa.chunk.js"
+  },
+  {
+    "revision": "5d6a0792971cee206099",
+    "url": "/woorian/DIYPAGE.git/static/js/runtime~main.dd592c73.js"
+  },
+  {
+    "revision": "5fbd7aec0d206f962613",
+    "url": "/woorian/DIYPAGE.git/static/js/2.8e8944f3.chunk.js"
+  },
+  {
+    "revision": "32d491a351da759af9976e601955b7cb",
+    "url": "/woorian/DIYPAGE.git/static/media/base.32d491a3.png"
+  },
+  {
+    "revision": "0eed6f715f88cee61e09502ca91a7f16",
+    "url": "/woorian/DIYPAGE.git/static/media/camera.0eed6f71.png"
+  },
+  {
+    "revision": "9fbc7a4c932fae13e6d222037885e9cc",
+    "url": "/woorian/DIYPAGE.git/static/media/example.9fbc7a4c.jpg"
+  },
+  {
+    "revision": "a2bdf0a9e901065b14b48374f13ffe3b",
+    "url": "/woorian/DIYPAGE.git/static/media/1t.a2bdf0a9.png"
+  },
+  {
+    "revision": "576e7507c2a25db95f6788dc7ddb35ef",
+    "url": "/woorian/DIYPAGE.git/static/media/1.576e7507.png"
+  },
+  {
+    "revision": "417bfca8cf74a2555a2be8ecde51b529",
+    "url": "/woorian/DIYPAGE.git/index.html"
+  }
+];
